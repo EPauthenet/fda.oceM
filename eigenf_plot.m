98d1563378ab82302d418f6c0175d28d4393d592
Etienne Pauthenet <etienne.pauthenet@gmail.com>
2019-06-19 11:13:28 +0200

    sign*

--- eigenf_plot.m
+++ eigenf_plot.m
@@ -39,8 +39,8 @@ function eigenf_plot(pca,te,sign)
   for k = 1:ndim
     d  = ((k-1)*nbas+1):(k*nbas);                                   % Position of the variable k in the eigenvectors
     pb = round(100*sum(pca.vecnotWM(d,te).^2),0);                   % Percentage of the bloc
-    fdobj_vp = fd(Cm(d)' + pca.axes(d,te),pca.basis,pca.fdnames);
-    fdobj_vm = fd(Cm(d)' - pca.axes(d,te),pca.basis,pca.fdnames);
+    fdobj_vp = fd(Cm(d)' + sign*pca.axes(d,te),pca.basis,pca.fdnames);
+    fdobj_vm = fd(Cm(d)' - sign*pca.axes(d,te),pca.basis,pca.fdnames);
     fdobj_m   = fd(Cm(d)',pca.basis,pca.fdnames);
     vp = eval_fd(depth,fdobj_vp);
     vm = eval_fd(depth,fdobj_vm);
